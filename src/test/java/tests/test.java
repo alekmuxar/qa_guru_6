612b2535ff2ee8e70fc47fa99cceac4da75e1d58
Александр <alekmuxar@gmail.com>
2021-07-29 13:17:18 +0500

    Тест для задания по Jenkins

--- src/test/java/tests/test.java
+++ src/test/java/tests/test.java
@@ -1,34 +1,20 @@
 package tests;
 
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.Selectors;
-import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverRunner.driver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class test {
+
     @Test
-    void findTest() {
+    void positiveTest() {
 
-        open("https://vk.com/");
-        $("#index_email").click();
-        $("#index_email").val("xxxxx");
-        $("#index_pass").click();
-        $("#index_pass").val("xxxxx");
-        $("#index_login_button").click();
-        sleep(3000);
-        $(".side_bar_inner").$(byText("Файлы")).click();
-        sleep(3000);
-        ElementsCollection files = $$(".docs_action_icon.docs_delete_row");
+        assertEquals(3-2, 1);
+    }
 
-        for (SelenideElement element: files) {
-            element.click();
-        }
+    @Test
+    void negativeTest() {
+
+        assertEquals(2-1, 0);
     }
 }
